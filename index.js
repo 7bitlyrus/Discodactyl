f267860010e8f89a269d5034b546bf9aaee6f858
Ian Murray <IanMurray@users.noreply.github.com>
2019-12-02 08:33:33 -0500

    Fix arrow bracket chat issue and config key name (#1)
    * Fix issue with arrow brackets in message
    
    * Update consts config override keyname

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const config = require('./config.json')
 
-const consts = require('./consts.js')(config.regex || {})
+const consts = require('./consts.js')(config.consts || {})
 const pterodactyl = require('./clients/pterodactyl.js')(config.pterodactyl || {})
 const discord = require('./clients/discord.js')(config.discord || {})
 
