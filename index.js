4648eda4d20ed36be104625b34f81d5724a2ea0b
Ian Murray <theianm2002@ymail.com>
2019-12-11 21:02:05 -0500

    Re-merge without pending merge

--- index.js
+++ index.js
@@ -5,7 +5,4 @@ const pterodactyl = require('./clients/pterodactyl.js')(config.pterodactyl || {}
 const discord = require('./clients/discord.js')(config.discord || {})
 
 require('./bridge')(discord, pterodactyl, consts)
-<<<<<<< HEAD
 require('./misc/memWatch.js')(pterodactyl, config.memlimit || NaN)
-=======
->>>>>>> 01c4a93bef345023a8891d77125c55cb5d6da825
